3e76711f8a18c9e14f7c09a706e758543818d83c
Jaime Bohorquez-Ballen <jbohorqu@gmail.com>
2024-12-17 11:27:20 -0500

    A bunch of changes .. Didn't keep track of all

--- assets/js/col_menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("col_menu").style.marginLeft = "250px";
-}
-  
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("col_menu").style.marginLeft= "0";
-}
---
+++ js/col_menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("col_menu").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("col_menu").style.marginLeft= "0";
+}
